9e678c8eb437449080b748a2efeff8b9c8c944ba
Simon Newman <si.newman87@gmail.com>
2017-10-25 11:29:37 +0100

    Change logo.png to svg

--- themes/Bedrock/header.php
+++ themes/Bedrock/header.php
@@ -10,7 +10,6 @@
 
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen">
 		<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:400,700" rel="stylesheet">
-
 		<link rel="icon" href="<?php bloginfo('template_url'); ?>/favicon.png" type="image/png">
 
 		<?php wp_head(); ?>
@@ -24,7 +23,7 @@
 			<div class="container">
 				<div class="open-menu-btn"><span></span></div>
 					<div class="logo">
-						<a href="/"><img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/logo.png"></a>
+						<a href="/"><img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/logo.svg"></a>
 					</div>
 					<nav><?php wp_nav_menu( array( 'theme_location' => 'main') ); ?></nav>
 				</div>
