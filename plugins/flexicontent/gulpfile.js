769ac7747bb0b8d7bef98b57c8b778576936ea23
Simon Newman <si.newman87@gmail.com>
2018-03-19 15:13:23 +0000

    Added Flexi Content plugin

---
+++ plugins/flexicontent/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(''))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('scss/*.scss', ['sass'])
+});
